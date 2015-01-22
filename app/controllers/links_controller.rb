722815a2f14ad8c19ea50b675a2b0e6bbfc65756
Tim Renken <tim@timrenken.io>
2015-01-21 21:16:46 -0600

    added color to buttons, fixed upvote and downvote friendly id

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -68,13 +68,13 @@ class LinksController < ApplicationController
 end
 	
 def upvote
-  @link = Link.find(params[:id])
+  @link = Link.friendly.find(params[:id])
   @link.upvote_by current_user
   redirect_to :back
 end
  
 def downvote
-  @link = Link.find(params[:id])
+  @link = Link.friendly.find(params[:id])
   @link.downvote_by current_user
   redirect_to :back
 end
